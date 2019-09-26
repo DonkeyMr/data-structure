112e5e25e660c9486e599d78d347be90af0ea7d8
liupeng <liupeng@bjzjmy.com>
2019-09-26 18:17:39 +0800

    二分搜索树添加元素

--- src/com/data/binarysearchtree/BST.java
+++ src/com/data/binarysearchtree/BST.java
@@ -4,7 +4,7 @@ package com.data.binarysearchtree;
  * @author liupeng
  * @date 2019/9/26
  */
-public class BST<E> {
+public class BST<E extends Comparable<E>> {
 
     private class Node {
         public E e;
@@ -30,5 +30,29 @@ public class BST<E> {
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    /**
+     * 向二分搜索树中添加元素
+     */
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    /**
+     * 返回插入新节点后的二分搜索树的根
+     */
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0){
+            node.right = add(node.right, e);
+        }
+        return node;
     }
 }
