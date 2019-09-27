d36564bc3e903ef7a30833dd9f5d7a736d2d05a8
liupeng <liupeng@bjzjmy.com>
2019-09-27 17:21:17 +0800

    非递归前序遍历

--- src/com/data/binarysearchtree/BSTTest.java
+++ src/com/data/binarysearchtree/BSTTest.java
@@ -8,15 +8,13 @@ public class BSTTest {
 
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
-        int[] arr = {5, 3, 6, 8, 4, 2};
+        int[] arr = {36, 27, 45, 18, 30, 46};
         for (int i : arr) {
             bst.add(i);
         }
 
         bst.preOrder();
         System.out.println();
-        bst.inOrder();
-        System.out.println();
-        bst.postOrder();
+        bst.preOrderNR();
     }
 }
