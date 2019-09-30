8c820a483d2c9b9c0a194539eb72fd92ab1da2d6
liupeng <liupeng@bjzjmy.com>
2019-09-30 16:15:56 +0800

    BSTMap

--- src/com/data/map/BSTMap.java
+++ src/com/data/map/BSTMap.java
@@ -4,5 +4,168 @@ package com.data.map;
  * @author liupeng
  * @date 2019/9/30
  */
-public class BSTMap {
+public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node left;
+        public Node right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    private int size;
+
+    public BSTMap() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    /**
+     * 添加元素
+     */
+    @Override
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
+
+    /**
+     * 返回插入新节点后的二分搜索树的根
+     */
+    private Node add(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+
+        if (key.compareTo(node.key) < 0) {
+            node.left = add(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = add(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+        return node;
+    }
+
+    @Override
+    public V remove(K key) {
+        Node node = getNode(root, key);
+        if (node == null) {
+            return null;
+        }
+
+        root = remove(node, key);
+        return node.value;
+    }
+
+    @Override
+    public void set(K key, V value) {
+        Node node = getNode(root, key);
+        if (node == null) {
+            throw new IllegalArgumentException(key + "doesn't existed");
+        }
+        node.value = value;
+    }
+
+    @Override
+    public V get(K key) {
+        Node node = getNode(root, key);
+        return node == null ? null : node.value;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return getNode(root, key) != null;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private Node getNode(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        if (key.equals(node.key)) {
+            return node;
+        } else if (key.compareTo(node.key) < 0) {
+            return getNode(node.left, key);
+        } else if (key.compareTo(node.key) > 0) {
+            return getNode(node.right, key);
+        }
+        return node;
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node right = node.right;
+            node.right = null;
+            size--;
+            return right;
+        }
+
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    private Node remove(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+            return node;
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+            return node;
+        } else {
+            if (node.left == null) {
+                Node right = node.right;
+                node.right = null;
+                size--;
+                return right;
+            }
+            if (node.right == null) {
+                Node left = node.left;
+                node.left = null;
+                size--;
+                return left;
+            }
+            /*
+            待删除节点左右子树均不为空的情况
+            找到比待删除节点大的最小节点，即待删除节点右子树的最小节点
+            用这个节点顶替待删除节点的位置
+             */
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            //   size++;
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            //    size--;
+            return successor;
+        }
+    }
 }
--- src/com/data/map/MapTest.java
+++ src/com/data/map/MapTest.java
@@ -20,7 +20,7 @@ public class MapTest {
         if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
             System.out.println(words.size());
 
-            LinkedListMap<String, Integer> map = new LinkedListMap<>();
+            Map<String, Integer> map = new BSTMap<>();
             words.forEach(word -> {
                 if (map.contains(word)) {
                     map.set(word, map.get(word) + 1);
