d630e00fdcf936bbce8ca90415b685a05c4c80a4
liupeng <liupeng@bjzjmy.com>
2019-12-03 16:30:42 +0800

    创建线段树

---
+++ src/com/data/segmentTree/SegmentTree.java
@@ -0,0 +1,46 @@
+package com.data.segmentTree;
+
+/**
+ * @author liupeng
+ * @date 2019/12/3
+ */
+public class SegmentTree<E> {
+
+    private Object[] data;
+
+    private Object[] tree;
+
+    public SegmentTree(E[] arr) {
+        data = new Object[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            data[i] = arr[i];
+        }
+
+        tree = new Object[4 * arr.length];
+    }
+
+    public int getSize() {
+        return data.length;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("index is illegal");
+        }
+        return (E) data[index];
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+     */
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+     */
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+}
